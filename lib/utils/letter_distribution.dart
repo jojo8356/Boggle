0787c07cb5d8b105e5ad5ec906d8b8696e240b7e
jojo8356 <polsinelli@gmx.com>
2025-12-26 16:23:57 +0100

    simulate real game with des

--- lib/utils/letter_distribution.dart
+++ lib/utils/letter_distribution.dart
@@ -1,40 +1,43 @@
 import 'dart:math';
 
 class LetterDistribution {
-  static const Map<String, int> frenchLetterWeights = {
-    'E': 15, 'A': 9, 'I': 8, 'S': 8, 'N': 7, 'R': 7, 'T': 7, 'O': 6,
-    'L': 6, 'U': 6, 'D': 4, 'C': 3, 'M': 3, 'P': 3, 'G': 2, 'B': 2,
-    'V': 2, 'H': 2, 'F': 2, 'Q': 1, 'Y': 1, 'X': 1, 'J': 1, 'K': 1,
-    'W': 1, 'Z': 1,
-  };
+  /// Les 16 dés officiels du Boggle français (6 faces par dé)
+  static const List<List<String>> boggleDice = [
+    ['E', 'T', 'U', 'K', 'N', 'O'],
+    ['E', 'V', 'G', 'T', 'I', 'N'],
+    ['D', 'E', 'C', 'A', 'M', 'P'],
+    ['I', 'E', 'L', 'R', 'U', 'W'],
+    ['E', 'H', 'I', 'F', 'S', 'E'],
+    ['R', 'E', 'C', 'A', 'L', 'S'],
+    ['E', 'N', 'T', 'D', 'O', 'S'],
+    ['O', 'F', 'X', 'R', 'I', 'A'],
+    ['N', 'A', 'V', 'E', 'D', 'Z'],
+    ['E', 'I', 'O', 'A', 'T', 'A'],
+    ['G', 'L', 'E', 'N', 'Y', 'U'],
+    ['B', 'M', 'A', 'Q', 'J', 'O'],
+    ['T', 'L', 'I', 'B', 'R', 'A'],
+    ['S', 'P', 'U', 'L', 'T', 'E'],
+    ['A', 'I', 'M', 'S', 'O', 'R'],
+    ['E', 'N', 'H', 'R', 'I', 'S'],
+  ];
 
-  static final List<String> _weightedLetters = _buildWeightedList();
-
-  static List<String> _buildWeightedList() {
-    final List<String> letters = [];
-    frenchLetterWeights.forEach((letter, weight) {
-      for (int i = 0; i < weight; i++) {
-        letters.add(letter);
-      }
-    });
-    return letters;
-  }
-
+  /// Génère une grille en mélangeant les 16 dés et en lançant chacun
   static List<String> generateGrid(int size) {
     final random = Random();
+    final int totalCells = size * size;
+
+    // Copier et mélanger les dés
+    final List<List<String>> shuffledDice = List.from(boggleDice);
+    shuffledDice.shuffle(random);
+
     final List<String> grid = [];
 
-    for (int i = 0; i < size * size; i++) {
-      grid.add(_weightedLetters[random.nextInt(_weightedLetters.length)]);
-    }
-
-    // Assurer au moins une voyelle
-    final vowels = ['A', 'E', 'I', 'O', 'U'];
-    bool hasVowel = grid.any((letter) => vowels.contains(letter));
-
-    if (!hasVowel) {
-      final randomIndex = random.nextInt(grid.length);
-      grid[randomIndex] = vowels[random.nextInt(vowels.length)];
+    for (int i = 0; i < totalCells; i++) {
+      // Utiliser le dé correspondant (modulo pour les grilles plus grandes)
+      final die = shuffledDice[i % shuffledDice.length];
+      // Lancer le dé (choisir une face au hasard)
+      final face = die[random.nextInt(die.length)];
+      grid.add(face);
     }
 
     return grid;
